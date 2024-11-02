1cc0c7c9a54157f17d52e5b06d659daf1de2d632
AdityaSharma42 <adityasharma3@usf.edu>
2024-11-02 19:23:55 -0400

    Format product information dynamically.

--- main.js
+++ main.js
@@ -8,12 +8,32 @@ fetch ('https://www.course-api.com/javascript-store-products')
     }
     return response.json();
 })
-.then (data=>{
-    displayProducts(data);
+.then (products=> {
+    products.forEach(product=>{
+        const listItem= document.createElement('li');
+        const productDetails= document.createElement('div');
+
+        const productImage= document.createElement('img');
+        productImage.src= product.fields.image[0].url;
+        productImage.alt=product.fields.name;
+
+        const productInfo=document.createElement('p');
+
+        productInfo.textContent=`${product.fields.name}- $${product.fields.price}`;
+       
+        const companyName= document.createElement('p');
+        companyName.textContent= `Company: ${product.fields.company}`;
+       
+        productDetails.appendChild(productImage);
+        productDetails.appendChild(productInfo);
+        productDetails.appendChild(companyName);
+        listItem.appendChild(productDetails);
+        productContainer.appendChild(listItem);
+    });
 })
        
 .catch (error=>{
-    errorMessage.textContent='Can not load products right now. Please try again later.';
     console.error ('There was a problem with the fetch operation:', error);
+    errorMessage.textContent='Can not load products right now. Please try again later.';
 });
 
