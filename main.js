ca78d9fb78c497f94a9cf049e9cc89a43210716f
AdityaSharma42 <adityasharma3@usf.edu>
2024-11-02 20:35:16 -0400

    Handle errors with .catch() and display error message.

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 const productContainer= document.getElementById('product-container');
 const errorMessage= document.getElementById('error-message');
 
-fetch ('https://www.course-api.com/javascript-store-products')
-.then (response =>{
+fetch ('https://www.course-api.com/javascript-store-products') //fetching product data from the api
+.then (response =>{ //checking if the API responds properly
     if (!response.ok){
-        throw new Error('Error');
+        throw new Error('Error');//the error is thrown to be caught if not
     }
     return response.json();
 })
 .then (products=> {
     products.forEach(product=>{
-        const listItem= document.createElement('li');
+        const listItem= document.createElement('li'); 
         const productDetails= document.createElement('div');
+        productDetails.className= 'product-details';
 
-        const productImage= document.createElement('img');
-        productImage.src= product.fields.image[0].url;
-        productImage.alt=product.fields.name;
+        const productImage= document.createElement('img'); //creating an image element
+        productImage.src= product.fields.image[0].url; //the source is the url
+        productImage.alt=product.fields.name; //alt text
 
         const productInfo=document.createElement('p');
-
-        productInfo.textContent=`${product.fields.name}- $${product.fields.price}`;
+        productInfo.textContent=`Product: ${product.fields.name}- $${product.fields.price}`;
        
         const companyName= document.createElement('p');
-        companyName.textContent= `Company: ${product.fields.company}`;
+        companyName.textContent= `Company: ${product.fields.company}`;//making the text include the name of the company
        
         productDetails.appendChild(productImage);
         productDetails.appendChild(productInfo);
         productDetails.appendChild(companyName);
+
         listItem.appendChild(productDetails);
         productContainer.appendChild(listItem);
     });
 })
        
 .catch (error=>{
-    console.error ('There was a problem with the fetch operation:', error);
-    errorMessage.textContent='Can not load products right now. Please try again later.';
+    console.error ('There was a problem with the fetch operation:', error); //if there is an error, it will be logged 
+    errorMessage.textContent='Can not load products right now. Please try again later.'; //error message
 });
 
